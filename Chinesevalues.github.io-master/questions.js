611fa3dbcf5237b6c15f2301b119cb92cebb3fc7
Mikhail Celflosky <92515833+Sykar2Socialist@users.noreply.github.com>
2022-06-11 19:09:18 +0800

    Add files via upload

---
+++ Chinesevalues.github.io-master/questions.js
@@ -0,0 +1,498 @@
+questions = [
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by CCP.",
+        "effect": {
+            "econ": 10,
+            "nati": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is necessary for the government to revive Han-nationalism.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "The freer the markets, the freer the people.",
+        "effect": {
+            "econ": -10,
+            "nati": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is better to maintain a balanced budget than to ensure welfare for all citizens.",
+        "effect": {
+            "econ": -10,
+            "nati": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Publicly-funded research is more beneficial to the people than leaving it to the market.",
+        "effect": {
+            "econ": 10,
+            "nati": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "We should follow Xi's policies.(This Question won't count Han-nationalism in.)",
+        "effect": {
+            "econ": 5,
+            "nati": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "We should bring back people's commune",
+        "effect": {
+            "econ": 10,
+            "nati": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It would be best if free healthcare  were abolished.",
+        "effect": {
+            "econ": -10,
+            "nati": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Taxes should be increased on the rich to provide for the poor.",
+        "effect": {
+            "econ": 10,
+            "nati": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Land is a legitimate form of wealth, there's no force removed.",
+        "effect": {
+            "econ": -10,
+            "nati": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Basic utilities like roads and electricity should be publicly owned.",
+        "effect": {
+            "econ": 10,
+            "nati": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Quality education is a right of all people.",
+        "effect": {
+            "econ": 10,
+            "nati": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Hengshui model of education is people's basic rights",
+        "effect": {
+            "econ": -5,
+            "nati": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "The 996 should banned for the workers.",
+        "effect": {
+            "econ": 10,
+            "nati": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is important to maintain Chinese sovereignty.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Other Nation's government would let China better.",
+        "effect": {
+            "econ": 0,
+            "nati": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is more important to retain peaceful relations than to further our Han-Supremacy.",
+        "effect": {
+            "econ": 0,
+            "nati": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Taiwan, Xinjiang, Tibet, Hong Kong is a part of PRC.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Taiwan, Xinjiang, Tibet, Hong Kong is a part of ROC ",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Wolf warrior diplomacy is not a waste of money.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Han nation is great.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    
+    {
+        "question": "Han nation's values should be spread as much as possible.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is very important to maintain Rule of CCP.",
+        "effect": {
+            "econ": 0,
+            "nati": -5,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "The general populace is too bad to vote.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "We won't forget Tiananmen Square Massacre.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The sacrifice of some civil liberties is necessary to protect Communist party of China.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "People's Democracy of CCP is really Democracy.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Massive surveillance is necessary in China.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "At least millions of people died from CCP.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The Chinese Communist party lead us liberated.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Minorities in China are stuck in Hellhole.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "CCP brought economic development, this is based. ",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Democracy or Authoritarian Democracy is better than People's Democracy in China.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    }
+    {
+        "question": "We should change words to be inclusive. ",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "People shouldn't have education, only nobility.",
+        "effect": {
+            "econ": -5,
+            "nati": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Chinese Traditions are of no value on their own.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Taoism or Chinese folk religion should play a role in government.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Statues of Saints should be replaced to Chairman Mao's face."
+        "effect": {
+            "econ": 5,
+            "nati": 0,
+            "govt": -10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Society was better before Xinhai Revolution than it is now.",
+        "effect": {
+            "econ": -5,
+            "nati": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "It is important that we maintain the traditional cultures of our history.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Drug use should be legalized or decriminalized.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "question": "Same-sex marriage should be legal.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 10,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Han Culture are superior to others.",
+        "effect": {
+            "econ": 0,
+            "nati": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Sex outside marriage is immoral.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "If we accept migrants at all, it is important that they assimilate into our culture.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I'm a woman, so I shouldn't work and my boyfriend will do it.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Prostitution should be illegal.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Prostitution is legal to CCP's bureaucrats.(Irony)",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Women shouldn't work",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Bring Back Tang Dynasty's honor.",
+        "effect": {
+            "econ": 0,
+            "nati": 10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "We should bring back Zhou Diyasty's feudalism and hierarches.",
+        "effect": {
+            "econ": 0,
+            "nati": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "We should open our borders to immigration.",
+        "effect": {
+            "econ": 0,
+            "nati": 10,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "We shouldn't discriminate Japanese and Americans",
+        "effect": {
+            "econ": 0,
+            "nati": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "All people - regardless of factors like culture or sexuality - should be treated equally.",
+        "effect": {
+            "econ": 10,
+            "nati": 5,
+            "govt": 5,
+            "scty": 5
+        }
+    },
+    {
+        "question": "It is important that we further CCP's goals above all others.",
+        "effect": {
+            "econ": -5,
+            "nati": -5,
+            "govt": -10,
+            "scty": -5
+        }
+    }
+];
